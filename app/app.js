4dc8c586d5c35a46b5ae95680a4a6e9e0f27a020
itsjustaplant <alperensahin442@gmail.com>
2020-12-23 00:01:42 +0300

    Add network check

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 const spotify = require('spotify-node-applescript')
+
 let color_base_url = "https://colorflyv1.herokuapp.com/v1/color/"
 let lyrics_base_url = "https://colorflyv1.herokuapp.com/v1/lyrics/"
 
@@ -31,7 +32,8 @@ const getLyrics = (api_url, opt) =>{
 const renderPage = () =>{
     spotify.getTrack((err, track) =>{
         let track_name = track['name']
-        if (document.getElementById('track_name').textContent !== track_name){
+        let check_name = track['artist'] + " - " + track_name
+        if (document.getElementById('track_name').textContent !== check_name){
             let image_url = track['artwork_url']
             let api_url = color_base_url + image_url
             let artist_name = track['artist']
@@ -43,6 +45,12 @@ const renderPage = () =>{
             getLyrics(lyrics_base_url, options)
             document.getElementById('cover_art').src = image_url
             document.getElementById('track_name').textContent = artist_name + " - " + track_name
+        } else if(!navigator.onLine){
+            document.getElementById('status').textContent = "🙈mayday we've lost connection🙈"
+            document.getElementById('status_bar').style.backgroundColor = '#ff5c5c'
+            document.getElementById('status_bar').style.animation = 'reverse-snap 0.5s forwards'
+        } else if(navigator.onLine){
+            document.getElementById('status_bar').style.animation = 'snap 0.5s forwards'
         }
     })
 }
